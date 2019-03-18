e74a78402bb5a1ef490d30b7befd4c49e9abe5e3
svgnAyure <kristian.sta@gmail.com>
2019-03-18 04:54:05 +0100

    Added global styles.

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,11 @@ import { ApolloProvider } from 'react-apollo-hooks'
 
 import client from './apollo'
 import Routes from './routes'
+import Styles from './styles'
 
 const App = (
   <ApolloProvider client={client}>
+    <Styles />
     <Routes />
   </ApolloProvider>
 )
---
+++ client/src/styles.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  body {
+    background: #eee;
+    margin: 0;
+  }
+`
+
+export default GlobalStyle
